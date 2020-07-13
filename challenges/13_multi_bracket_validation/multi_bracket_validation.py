84140db7a1e1d3e9698a65a0a6a28194809debc3
vorSherer <tcpma2011@gmail.com>
2020-07-12 21:24:02 -0700

    Began CC-13 work.

--- challenges/13_multi_bracket_validation/multi_bracket_validation.py
+++ challenges/13_multi_bracket_validation/multi_bracket_validation.py
@@ -1,3 +1,10 @@
+# Your function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+
+# Round Brackets : ()
+# Square Brackets : []
+# Curly Brackets : {}
+
+
 def multi_bracket_validation(input):
     """
     docstring here.
@@ -6,3 +13,11 @@ def multi_bracket_validation(input):
         self.input = input
 
     pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
